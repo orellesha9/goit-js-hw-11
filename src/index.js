4b0eb57969f4429e03e44dd1a2e42d86aeb5e5b2
orellesha9 <orellesha9@gmail.com>
2023-11-26 22:44:35 +0200

    first commit2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
+import { searchElemenets } from './search-api';
 
 
+searchElemenets()
+// console.log(searchElemenets(`котики`));
 const refs = {
   btnSubmit: document.querySelector(`#search-form`)
 };
--- src/search-api.js
+++ src/search-api.js
@@ -1,3 +1,26 @@
 import axios from 'axios';
+axios.defaults.baseURL =
+  'https://pixabay.com/api/?key=11809160-30735c542d1a44d0753f02a93&image_type=photo&orientation=horizontal&safesearch=true&q=';
+axios.defaults.headers.common['key'] = '11809160-30735c542d1a44d0753f02a93';
+
+
+
+export async function searchElemenets(search) { 
+   try {
+       const { data } = await axios.get(
+         `https://pixabay.com/api/?key=11809160-30735c542d1a44d0753f02a93&image_type=photo&orientation=horizontal&safesearch=true&q=${search}`
+       );
+        return data;
+   } catch (error) {
+    console.log(error);
+   }
+   
+    
+    
+   
+    
+    }
+    
+
 
 
