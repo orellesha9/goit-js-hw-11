bb16e2dd9f79c72d9f89a54d10e02a74aa9d7b25
orellesha9 <orellesha9@gmail.com>
2023-11-27 00:56:53 +0200

    first commit5

--- src/index.js
+++ src/index.js
@@ -12,7 +12,36 @@ const refs = {
 
 searchElements(["котики"])
   .then(data => {
-    console.log('Отримані дані:', data);
+  result =  data
+     .map(
+       ({
+         webformatURL,
+         largeImageURL,
+         tags,
+         likes,
+         views,
+         comments,
+         downloads,
+       }) => `<div class="photo-card">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes ${likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views ${views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments ${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads ${downloads}</b>
+    </p>
+  </div>
+</div>`
+   )
+
+     .join('');
   })
   .catch(error => {
     console.error('Sorry, there are no images matching your search query. Please try again.');
@@ -21,3 +50,4 @@ searchElements(["котики"])
 
 
 
+console.log(searchElements(['котики']));
--- src/search-api.js
+++ src/search-api.js
@@ -7,10 +7,10 @@ import axios from 'axios';
 
 export async function searchElements(arg) {
   try {
-    const response = await axios.get(
+    const {data} = await axios.get(
       'https://pixabay.com/api/?key=11809160-30735c542d1a44d0753f02a93&image_type=photo&orientation=horizontal&safesearch=true&q=${arg}'
     );
-    return response.data;
+    return data;
   } catch (error) {
     console.error('Помилка:', error);
     throw new Error('Помилка отримання даних через axios');
