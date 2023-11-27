bf5d45ca02f35c0aebd5a501127161d26fa7451f
orellesha9 <orellesha9@gmail.com>
2023-11-27 16:59:40 +0200

    get_request1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
 import { searchElements } from './search-api';
+import { createMarkup } from './search-api';
 
 const refs = {
   btnSubmit: document.querySelector(`#search-form`),
@@ -15,40 +16,10 @@ function handleSubmit(event) {
   currentInput = refs.input.value;
   console.log(currentInput);
 
-  searchElements(currentInput).then(data => {
-    result = data
-      .map(
-        ({
-          webformatURL,
-          largeImageURL,
-          tags,
-          likes,
-          views,
-          comments,
-          downloads,
-        }) => `<div class="photo-card">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>Likes ${likes}</b>
-    </p>
-    <p class="info-item">
-      <b>Views ${views}</b>
-    </p>
-    <p class="info-item">
-      <b>Comments ${comments}</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads ${downloads}</b>
-    </p>
-  </div>
-</div>`
-      )
-      .join('');
-  })
-  .catch(error => {
-    console.log(
-      'Sorry, there are no images matching your search query. Please try again.'
-    );
-  })
-}
+  searchElements(currentInput)
+    .then(data => (refs.gallery.innerHTML = createMarkup(data)))
+    .catch(error => {
+      console.log(error);
+    });
+    
+  }
--- src/search-api.js
+++ src/search-api.js
@@ -6,18 +6,35 @@ import axios from 'axios';
 
 
 export async function searchElements(arg) {
-  // try {
   const { data } = await axios.get(
-    'https://pixabay.com/api/?key=11809160-30735c542d1a44d0753f02a93&image_type=photo&orientation=horizontal&safesearch=true&q=${arg}'
+    `https://pixabay.com/api/?key=11809160-30735c542d1a44d0753f02a93&image_type=photo&orientation=horizontal&safesearch=true&q=${arg}`
   );
   console.log(data);
   return data;
-
 }
-  // } catch (error) {
-//     console.error('Помилка:', error);
-//     throw new Error('Помилка отримання даних через axios');
-//   }
-// }
 
+
+
+export function createMarkup(arr) {
+  return arr.hits.map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `
+      <div class="photo-card">
+        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+        <div class="info">
+          <p class="info-item"><b>Likes ${likes}</b></p>
+          <p class="info-item"><b>Views ${views}</b></p>
+          <p class="info-item"><b>Comments ${comments}</b></p>
+          <p class="info-item"><b>Downloads ${downloads}</b></p>
+        </div>
+      </div>`)
+    .join('');
+}
 
